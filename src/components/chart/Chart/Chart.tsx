57eb97016df4d1963e195ec2efc852156aa9cac1
Rhys Halsey-Watkins <rhyshalsey@gmail.com>
2022-09-23 20:33:50 -0400

    Completed dynamically drawn chart

--- src/components/chart/Chart/Chart.tsx
+++ src/components/chart/Chart/Chart.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import { useSelector } from "react-redux";
 import { mint, slate } from "@radix-ui/colors";
@@ -15,7 +15,12 @@ const Chart = () => {
     height: 0,
   });
 
+  const windowUpdateTimeoutRef = useRef<ReturnType<typeof setTimeout>>();
+
   const chartRef = useRef(null);
+
+  const chartDrawIndexRef = useRef(0);
+  const chartDrawIntervalRef = useRef<ReturnType<typeof setInterval>>();
 
   const currentCoinId = useSelector(
     (state: RootState) => state.app.currentCoinId
@@ -31,14 +36,20 @@ const Chart = () => {
     14
   );
 
-  // console.log(coinMarketData);
-
   useEffect(() => {
     const updateWindowDimensions = () => {
-      setWindowDimensions({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
+      if (windowUpdateTimeoutRef.current) {
+        clearTimeout(windowUpdateTimeoutRef.current);
+      }
+
+      // Because drawing the chart dynamically is intensive,
+      // only update the window dimensions after a short delay
+      windowUpdateTimeoutRef.current = setTimeout(() => {
+        setWindowDimensions({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }, 500);
     };
 
     window.addEventListener("resize", updateWindowDimensions);
@@ -50,7 +61,7 @@ const Chart = () => {
     };
   }, []);
 
-  useEffect(() => {
+  const drawChart = useCallback(() => {
     if (
       !coinMarketData?.prices ||
       coinMarketData.prices.length <= 0 ||
@@ -132,9 +143,14 @@ const Chart = () => {
     const xAxisElem = chartElem.select<SVGSVGElement>(".xAxis");
     xAxisElem.selectAll("*").remove();
 
+    const xAxis = d3
+      .axisBottom(x)
+      .ticks(10)
+      .tickFormat((d) => d3.timeFormat("%d %a")(d as Date));
+
     xAxisElem
       .attr("transform", `translate(0, ${windowDimensions.height - 20})`)
-      .call(d3.axisBottom(x));
+      .call(xAxis);
 
     const xAxisTicks = xAxisElem.selectAll(".tick");
 
@@ -157,25 +173,115 @@ const Chart = () => {
     xAxisElem.selectAll(".tick").selectAll("line").remove();
     xAxisElem.selectAll(".domain").remove();
 
-    // Create chart's price line
+    if (chartDrawIntervalRef.current) {
+      clearInterval(chartDrawIntervalRef.current);
+      chartDrawIndexRef.current = 0;
+    }
+
+    // This will make the chart draw one point at a time to make it look animated
+    // Creates an interval which adds a point to the line data array
+    chartDrawIntervalRef.current = setInterval(() => {
+      // Make sure we don't go past the price data length
+      chartDrawIndexRef.current = Math.min(
+        chartDrawIndexRef.current + 1,
+        priceData.length
+      );
+
+      const currentPriceData = priceData.slice(0, chartDrawIndexRef.current);
+
+      if (
+        chartDrawIntervalRef.current &&
+        chartDrawIndexRef.current >= priceData.length
+      ) {
+        clearInterval(chartDrawIntervalRef.current);
+        chartDrawIndexRef.current = 0;
+      }
+
+      // Create chart's price line
+      const line = d3
+        .line(coinMarketData.prices)
+        .x((d) => {
+          return x(new Date(d[0]));
+        })
+        .y((d) => {
+          return y(d[1]);
+        });
+
+      chartElem
+        .select("path")
+        .datum(currentPriceData)
+        .attr("class", "chartLine")
+        .attr("fill", "none")
+        .attr("stroke", mint.mint11)
+        .attr("stroke-width", 2)
+        .attr("d", line);
+    }, 0.5);
+  }, [coinMarketData, currentCurrency, windowDimensions]);
+
+  const drawLoadingChart = useCallback(() => {
+    const chartElem = d3.select(chartRef.current);
+
+    const fakeData: [number, number][] = Array.from(Array(250)).map(
+      (_v, index) => [Math.random(), index]
+    );
+    console.log(fakeData);
+
+    // Create y axis
+    const yRange = d3.extent(
+      fakeData,
+      (d: [number, number]) => d[0]
+    ) as number[];
+
+    const y = d3
+      .scaleLinear()
+      .domain(yRange)
+      .range([windowDimensions.height - 30, 0]);
+
+    const yAxisElem = chartElem.select<SVGSVGElement>(".yAxis");
+    yAxisElem.selectAll("*").remove();
+
+    // Dynamically set yAxis to align to left of parent
+    const yAxisWidth = yAxisElem.node()?.getBBox().width || 60;
+    yAxisElem.attr("transform", `translate(${yAxisWidth}, 0)`);
+
+    // Create x axis
+    const xRange = d3.extent(
+      fakeData,
+      (d: [number, number]) => d[1]
+    ) as number[];
+
+    const x = d3
+      .scaleLinear()
+      .domain(xRange)
+      .range([yAxisWidth, windowDimensions.width]);
+
     const line = d3
-      .line(coinMarketData.prices)
+      .line(fakeData)
       .x((d) => {
-        return x(new Date(d[0]));
+        return x(d[1]);
       })
       .y((d) => {
-        return y(d[1]);
+        return y(d[0]);
       });
 
     chartElem
       .select("path")
-      .datum(priceData)
+      .datum(fakeData)
       .attr("class", "chartLine")
       .attr("fill", "none")
       .attr("stroke", mint.mint11)
       .attr("stroke-width", 2)
       .attr("d", line);
-  }, [coinMarketData, currentCurrency, windowDimensions]);
+  }, [windowDimensions]);
+
+  // Chart is drawn in this useEffect
+  useEffect(() => {
+    if (isLoading) {
+      drawLoadingChart();
+    } else {
+      drawChart();
+    }
+  }, [drawChart, drawLoadingChart, isLoading]);
 
   return (
     <div className={styles.chartContainer}>
